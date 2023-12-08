356f5d05fbae637885fd5151f80460010d91bc68
Kingsley Okpo <kngsley2018@gmail.com>
2023-12-08 17:55:35 +0100

    fixes bugs in dispatcher.c

--- dispatcher.c
+++ dispatcher.c
@@ -4,15 +4,24 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
+#include <string.h>
 void dispatcher(char *str)
 {
 	int i = 0;
 	pid_t id = fork();
+	
+	char *dest = malloc(sizeof(char) * strlen(str) + 1);
+	while (str[i] != '\0' && str[i] != '\n')
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
 	if (id == 0)
 	{
-		execlp(str, "ls", (char *)NULL);
+		execlp(dest, dest, (char*)NULL);
 	}
-
-	wait(0);
+	free(dest);
+	wait(NULL);
 	
 }

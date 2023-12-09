e917838d71be1f3f8483b97678bb7b577dfc6019
Gift <aghaulor.gift@gmail.com>
2023-12-09 13:50:34 -0800

    Updates str_chr.c doc

--- str_chr.c
+++ str_chr.c
@@ -1,10 +1,11 @@
 #include "shell.h"
 #include <stdio.h>
+
 /**
- *_strchr - finds a value within a string
- *@s: this is the string to loop over
- *@c: this is the value to find
- *Return: address of the first match
+ * _strchr - Finds a value within a string
+ * @s: The string to loop over
+ * @c: The value to find
+ * Return: Address of the first match
  */
 char *str_chr(const char *s, int c)
 {

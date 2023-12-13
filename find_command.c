51b5fa23ba81785f1b73c207f0ee33c0e19cb952
Gift <aghaulor.gift@gmail.com>
2023-12-13 06:05:16 -0800

    Checked error in find_command.c

--- find_command.c
+++ find_command.c
@@ -11,49 +11,51 @@
  */
 char *find_command(const char *command)
 {
-    char *token;
-    char *exec_path;
-    char *path = getenv("PATH");
-    char *path_copy;
+	char *token;
+	char *exec_path;
+	char *path = getenv("PATH");
+	char *path_copy;
 
-    if (path == NULL)
-        return NULL;
+	if (path == NULL)
+	return (NULL);
 
-    // printf("%s\n", path);  // Debugging statement
+	printf("%s\n", path);
 
-    path_copy = strdup(path);
-    if (path_copy == NULL) {
-        perror("Memory allocation failed");
-        return NULL;
-    }
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+	{
+	perror("Memory allocation failed");
+	return (NULL);
+	}
 
-    token = strtok(path_copy, ":");
-    exec_path = (char *)malloc(strlen(token) + strlen(command) + 2);
+	token = strtok(path_copy, ":");
+	exec_path = (char *)malloc(strlen(token) + strlen(command) + 2);
 
-    if (exec_path == NULL) {
-        perror("Memory allocation failed");
-        free(path_copy);  // Free the duplicated path before returning
-        return NULL;
-    }
+	if (exec_path == NULL)
+	{
+	perror("Memory allocation failed");
+	free(path_copy);
+	return (NULL);
+	}
 
-    while (token != NULL)
-    {
-        strcpy(exec_path, token);
-        strcat(exec_path, "/");
-        strcat(exec_path, command);
-        
-        if (access(exec_path, X_OK) == 0) {
-            // printf("%s", exec_path);  // Debugging statement
-            free(path_copy);  // Free the duplicated path before returning
-	    printf("this is :%s\n",exec_path);
-            return exec_path;
+	while (token != NULL)
+	{
+	strcpy(exec_path, token);
+	strcat(exec_path, "/");
+	strcat(exec_path, command);
 
-        }
-        
-        token = strtok(NULL, ":");
-    }
+	if (access(exec_path, X_OK) == 0)
+	{
+	printf("%s", exec_path);
+	free(path_copy);
+	printf("this is :%s\n", exec_path);
+	return (exec_path);
+	}
 
-    free(exec_path);
-    free(path_copy);  // Free the duplicated path before returning
-    return NULL;
+	token = strtok(NULL, ":");
+	}
+
+	free(exec_path);
+	free(path_copy);
+	return (NULL);
 }

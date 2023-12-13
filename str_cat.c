93f5e01db724bcfefc027590f9fedead402549f4
Gift <aghaulor.gift@gmail.com>
2023-12-13 06:14:09 -0800

    Checked error in str_cat.c

--- str_cat.c
+++ str_cat.c
@@ -3,10 +3,10 @@
 #include <string.h>
 
 /**
- * _strcat - Concatenates two strings.
+ * str_cat - Concatenates two strings.
  * @dest: The first string.
  * @src: The second string.
-
+ *
  * Return: Modified string.
  */
 
@@ -18,7 +18,7 @@ char *str_cat(char *dest, const char *src)
 	k = strlen(src);
 	for (i = 0; i < k; i++)
 	{
-		*(dest + (i + j)) = src[i];
+	*(dest + (i + j)) = src[i];
 	}
 	*(dest + (i + j)) = '\0';
 	return (dest);

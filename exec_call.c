80999eab94fb62bee4e6410fc21d1818fecba9b8
Gift <aghaulor.gift@gmail.com>
2023-12-13 05:42:22 -0800

    Checked error in exec_call.c

--- exec_call.c
+++ exec_call.c
@@ -3,33 +3,40 @@
 #include <unistd.h>
 #include "shell.h"
 #include <sys/wait.h>
+
+/**
+ * exec_command- Execute commands.
+ * @command: A pointer that points to the address of the command.
+ *
+ * Return:Void
+ */
 void exec_command(char **command)
 {
 	char *prog_path;
+
 	prog_path = find_command(command[0]);
-    	if (prog_path == NULL)
-		return;
+	if (prog_path == NULL)
+	return;
 	pid_t pid = fork();
 	if (pid == -1)
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
+	perror("fork");
+	exit(EXIT_FAILURE);
 	}
 	else if (pid == 0)
 	{
-
-		if (execve(prog_path, command, NULL) == -1)
-		{
-			perror("execv");
-			exit(EXIT_FAILURE);
-		}
+	if (execve(prog_path, command, NULL) == -1)
+	{
+	perror("execv");
+	exit(EXIT_FAILURE);
+	}
 	}
 	else
 	{
-		int status;
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status)) 
-			printf("Command exited with status %d\n", WEXITSTATUS(status));
+	int status;
+	waitpid(pid, &status, 0);
+	if (WIFEXITED(status))
+	printf("Command exited with status %d\n", WEXITSTATUS(status));
 	}
 	free(prog_path);
 }

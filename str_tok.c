6f6c5db818571f489f1354e302e2a7ceb0e6f599
Gift <aghaulor.gift@gmail.com>
2023-12-13 06:42:33 -0800

    Checked error in str_tok.c

--- str_tok.c
+++ str_tok.c
@@ -5,39 +5,46 @@
 /**
  * str_tok- Tokenises the string.
  * @str: The string to tokenise.
- * @delimiter: The delimiter in the string.
+ * @delimiters: The delimiter in the string.
  *
  * Return:Tokenised string.
  */
 
-char *str_tok(char *str, const char *delimiters) {
-    static char *str_start = NULL;  // Static variable to keep track of the last token
-    char *initial_token;
-    
-    if (str == NULL)
-    {
-        str = str_start;
-    }
-    while (*str != '\0' && str_chr(delimiters, *str) != NULL)
-    {
-        str++;
-    }
-    if (*str == '\0') {
-        str_start = NULL;
-        return NULL;
-    }
+char *str_tok(char *str, const char *delimiters)
+{
+	static char *str_start;
 
-    
-    initial_token = str;
-    while (*str != '\0' && str_chr(delimiters, *str) == NULL) {
-        str++;
-    }
-    if (*str == '\0') {
-        str_start = str;
-    } else {
-        *str = '\0';
-        str_start = str + 1;
-    }
-    return (initial_token);
+	str_start = NULL;
+
+	char *initial_token;
+
+	if (str == NULL)
+	{
+	str = str_start;
+	}
+	while (*str != '\0' && str_chr(delimiters, *str) != NULL)
+	{
+	str++;
+	}
+	if (*str == '\0')
+	{
+	str_start = NULL;
+	return (NULL);
+	}
+
+	initial_token = str;
+	while (*str != '\0' && str_chr(delimiters, *str) == NULL)
+	{
+	str++;
+	}
+	if (*str == '\0')
+	{
+	str_start = str;
+	}
+	else
+	{
+	*str = '\0';
+	str_start = str + 1;
+	}
+	return (initial_token);
 }
-

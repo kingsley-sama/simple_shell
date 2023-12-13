e50db0699175903f64d71b3330965b157e69ab25
Gift <aghaulor.gift@gmail.com>
2023-12-13 05:51:45 -0800

    Checked error in exec_le.c

--- exec_le.c
+++ exec_le.c
@@ -1,23 +1,24 @@
-/* #include <stdio.h> */
-/* #include <stdlib.h> */
-/* #include <unistd.h> */
-/* /\** */
-/*  *exec_le - starts a child process */
-/*  *@path: path variable */
-/*  *@arguments: arguments */
-/*  *@envp: environment */
-/*  *Return: 0 if success */
-/*  *\/ */
-/* int exec_le(const char *path, char *const arguments[], char *const envp[]) */
-/* { */
-/* 	if (envp == NULL) */
-/* 	{ */
-/* 		envp = __environ; */
-/* 	} */
-/* 	if (execve(path, arguments, envp) == -1) */
-/* 	{ */
-/* 		perror("execve"); */
-/* 		return -1; */
-/* 	} */
-/* 	return (-1); */
-/* } */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * exec_le - starts a child process
+ * @path: path variable.
+ * @arguments: arguments.
+ * @envp: environment
+ * Return: 0 if success.
+ */
+int exec_le(const char *path, char *const arguments[], char *const envp[])
+{
+	if (envp == NULL)
+	{
+	envp = __environ;
+	}
+	if (execve(path, arguments, envp) == -1)
+	{
+	perror("execve");
+	return (-1);
+	}
+	return (-1);
+}

d45b35faea27572d7c195707454960140f2dd906
Gift <aghaulor.gift@gmail.com>
2023-12-15 11:02:59 -0800

    Checked betty error find_command.c

--- find_command.c
+++ find_command.c
@@ -2,8 +2,9 @@
 
 /**
  * path_cmd -  Search In $PATH for executable command
- * @cmd: Parsed input
- * Return: 0 on success or  1 on failure  0
+ * @cmd: Parsed input.
+ *
+ * Return: 0 on success or  1 on failure  0.
  */
 int path_cmd(char **cmd)
 {
@@ -14,16 +15,16 @@ int path_cmd(char **cmd)
 	value = str_tok(path, ":");
 	while (value != NULL)
 	{
-		command_path = build(*cmd, value);
-		if (stat(command_path, &buf) == 0)
-		{
-			*cmd = str_dup(command_path);
-			free(command_path);
-			free(path);
-			return (0);
-		}
-		free(command_path);
-		value = str_tok(NULL, ":");
+	command_path = build(*cmd, value);
+	if (stat(command_path, &buf) == 0)
+	{
+	*cmd = str_dup(command_path);
+	free(command_path);
+	free(path);
+	return (0);
+	}
+	free(command_path);
+	value = str_tok(NULL, ":");
 	}
 	free(path);
 	free(value);
@@ -34,6 +35,7 @@ int path_cmd(char **cmd)
  * build - Build command
  * @token: Executable command
  * @value: Directory conatining Command
+ *
  * Return: Parsed full path of command or NULL if failed
  */
 char *build(char *token, char *value)
@@ -45,8 +47,8 @@ char *build(char *token, char *value)
 	command_line = malloc(sizeof(char) * len);
 	if (command_line == NULL)
 	{
-		free(command_line);
-		return (NULL);
+	free(command_line);
+	return (NULL);
 	}
 
 	memset(command_line, 0, len);
@@ -61,6 +63,7 @@ char *build(char *token, char *value)
 /**
  * get_env - Gets the value of environment variable by name
  * @name: Environment variable name
+ *
  * Return: The value of the environment variable or NULL if failed
  */
 char *get_env(char *name)
@@ -72,26 +75,26 @@ char *get_env(char *name)
 	name_len = str_len(name);
 	for (i = 0 ; environ[i]; i++)
 	{
-		if (strn_cmp(name, environ[i], name_len) == 0)
-		{
-			value_len = str_len(environ[i]) - name_len;
-			value = malloc(sizeof(char) * value_len);
-			if (!value)
-			{
-				free(value);
-				perror("unable to alloc");
-				return (NULL);
-			}
+	if (strn_cmp(name, environ[i], name_len) == 0)
+	{
+	value_len = str_len(environ[i]) - name_len;
+	value = malloc(sizeof(char) * value_len);
+	if (!value)
+	{
+	free(value);
+	perror("unable to alloc");
+	return (NULL);
+	}
 
-			j = 0;
-			for (k = name_len + 1; environ[i][k]; k++, j++)
-			{
-				value[j] = environ[i][k];
-			}
-			value[j] = '\0';
+	j = 0;
+	for (k = name_len + 1; environ[i][k]; k++, j++)
+	{
+	value[j] = environ[i][k];
+	}
+	value[j] = '\0';
 
-			return (value);
-		}
+	return (value);
+	}
 	}
 	return (NULL);
 }

32d7631e09316c8745302c766beda6bff06e32d0
Gift <aghaulor.gift@gmail.com>
2023-12-15 12:33:48 -0800

    Fixed betty error in separator.c

--- separator.c
+++ separator.c
@@ -3,6 +3,7 @@
 /**
  * separator - Separates command recieved from stdin by ;
  * @input: String gathered from stdin
+ *
  * Return: Parsed strings to be used as commands
  */
 char **separator(char *input)
@@ -13,21 +14,21 @@ char **separator(char *input)
 	int buffsize = BUFSIZE;
 
 	if (input[0] == ' ' && input[str_len(input)] == ' ')
-		exit(0);
+	exit(0);
 	if (input == NULL)
-		return (NULL);
+	return (NULL);
 	commands = malloc(sizeof(char *) * buffsize);
 	if (!commands)
 	{
-		free(commands);
-		perror("hsh");
-		return (NULL);
+	free(commands);
+	perror("hsh");
+	return (NULL);
 	}
 	command = str_tok(input, ";&");
 	for (i = 0; command; i++)
 	{
-		commands[i] = command;
-		command = str_tok(NULL, ";&");
+	commands[i] = command;
+	command = str_tok(NULL, ";&");
 	}
 	commands[i] = NULL;
 

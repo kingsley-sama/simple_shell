6a25d3c000049110eea6f927d2faf85b2a683c1c
Gift <aghaulor.gift@gmail.com>
2023-12-15 12:56:09 -0800

    Fixed betty error in str_tok.c

--- str_tok.c
+++ str_tok.c
@@ -3,6 +3,7 @@
  * check_delim - function that checks if a character matchs any character
  * @c: character to check
  * @str: string of delimiters
+ *
  * Return: 1 on success, 0 on failure
  */
 unsigned int check_delim(char c, const char *str)
@@ -11,8 +12,8 @@ unsigned int check_delim(char c, const char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (c == str[i])
-			return (1);
+	if (c == str[i])
+	return (1);
 	}
 	return (0);
 }
@@ -21,6 +22,7 @@ unsigned int check_delim(char c, const char *str)
  * str_tok - function that extracts tokens of a string
  * @str: string
  * @delim: delimiter
+ *
  * Return: pointer to the next token or NULL
  */
 char *str_tok(char *str, const char *delim)
@@ -30,35 +32,35 @@ char *str_tok(char *str, const char *delim)
 	unsigned int i;
 
 	if (str != NULL)
-		new_token = str;
+	new_token = str;
 	tokens = new_token;
 	if (tokens == NULL)
-		return (NULL);
+	return (NULL);
 	for (i = 0; tokens[i] != '\0'; i++)
 	{
-		if (check_delim(tokens[i], delim) == 0)
-			break;
+	if (check_delim(tokens[i], delim) == 0)
+	break;
 	}
 	if (new_token[i] == '\0' || new_token[i] == '#')
 	{
-		new_token = NULL;
-		return (NULL);
+	new_token = NULL;
+	return (NULL);
 	}
 	tokens = new_token + i;
 	new_token = tokens;
 	for (i = 0; new_token[i] != '\0'; i++)
 	{
-		if (check_delim(new_token[i], delim) == 1)
-			break;
+	if (check_delim(new_token[i], delim) == 1)
+	break;
 	}
 	if (new_token[i] == '\0')
-		new_token = NULL;
+	new_token = NULL;
 	else
 	{
-		new_token[i] = '\0';
-		new_token = new_token + i + 1;
-		if (*new_token == '\0')
-			new_token = NULL;
+	new_token[i] = '\0';
+	new_token = new_token + i + 1;
+	if (*new_token == '\0')
+	new_token = NULL;
 	}
 	return (tokens);
 }

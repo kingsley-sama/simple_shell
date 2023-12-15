a3803d03841f8d32e80b19298208bb225bf2475d
Gift <aghaulor.gift@gmail.com>
2023-12-15 11:55:35 -0800

    Fixed betty error in mem_handle.c

--- mem_handle.c
+++ mem_handle.c
@@ -2,37 +2,38 @@
 
 /**
  * _realloc -  reallocates a memory block using malloc and free
- *@ptr: pointer
- *@old_size: previous Size Of The Pointer
- *@new_size: new size of the pointer
- *Return: Pointer to reallocated memory
+ * @ptr: pointer
+ * @old_size: previous Size Of The Pointer
+ * @new_size: new size of the pointer
+ *
+ * Return: Pointer to reallocated memory
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *result;
 
 	if (new_size == old_size)
-		return (ptr);
+	return (ptr);
 	if (new_size == 0 && ptr)
 	{
-		free(ptr);
-		return (NULL);
+	free(ptr);
+	return (NULL);
 	}
 	result = malloc(new_size);
 	if (result == NULL)
 	{
-		free(result);
-		return (NULL);
+	free(result);
+	return (NULL);
 	}
 	if (ptr == NULL)
 	{
-		fill_an_array(result, '\0', new_size);
-		free(ptr);
+	fill_an_array(result, '\0', new_size);
+	free(ptr);
 	}
 	else
 	{
-		_memcpy(result, ptr, old_size);
-		free(ptr);
+	_memcpy(result, ptr, old_size);
+	free(ptr);
 	}
 	return (result);
 }
@@ -41,6 +42,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
  * free_all - free memory allocated
  * @cmd: array pointer
  * @line: char pointer
+ *
  * Return: Void
  */
 void free_all(char **cmd, char *line)
@@ -53,10 +55,11 @@ void free_all(char **cmd, char *line)
 
 /**
  * _memcpy - copy bytes of memory from source to destination
- *@dest: destination pointer
- *@src: source pointer
- *@n: size to be copied
- *Return: pointer to destination
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: size to be copied
+ *
+ * Return: pointer to destination
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -64,17 +67,18 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+	dest[i] = src[i];
 	}
 	return (dest);
 }
 
 /**
  * fill_an_array - fill an array by constant byte
- *@a: void pointer
- *@el: int
- *@len: length for int
- *Return: void pointer
+ * @a: void pointer
+ * @el: int
+ * @len: length for int
+ *
+ * Return: void pointer
  */
 void *fill_an_array(void *a, int el, unsigned int len)
 {
@@ -83,17 +87,18 @@ void *fill_an_array(void *a, int el, unsigned int len)
 
 	while (i < len)
 	{
-		*p = el;
-		p++;
-		i++;
+	*p = el;
+	p++;
+	i++;
 	}
 	return (a);
 }
 
 /**
  * _calloc - allocates memory for an array using malloc
- *@size: size
- *Return: void pointer
+ * @size: size
+ *
+ * Return: void pointer
  */
 void *_calloc(unsigned int size)
 {
@@ -101,13 +106,13 @@ void *_calloc(unsigned int size)
 	unsigned int i;
 
 	if (size == 0)
-		return (NULL);
+	return (NULL);
 	a = malloc(size);
 	if (a == NULL)
-		return (NULL);
+	return (NULL);
 	for (i = 0; i < size; i++)
 	{
-		a[i] = '\0';
+	a[i] = '\0';
 	}
 	return (a);
 }

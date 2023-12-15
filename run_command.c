33a7b4ece5704a63d8004cd6143810789ad4ab66
Gift <aghaulor.gift@gmail.com>
2023-12-15 12:29:47 -0800

    Fixed betty error in run_command.c

--- run_command.c
+++ run_command.c
@@ -4,6 +4,7 @@
  * change_dir - Changes directory
  * @cmd: Parsed command
  * @st: Status of last command executed
+ *
  * Return: 0 on success 1 if failed (For OLDPWD Always 0 incase of no OLDPWD)
  */
 int change_dir(char **cmd, __attribute__((unused))int st)
@@ -12,24 +13,24 @@ int change_dir(char **cmd, __attribute__((unused))int st)
 	char cwd[PATH_MAX];
 
 	if (cmd[1] == NULL)
-		value = chdir(getenv("HOME"));
+	value = chdir(getenv("HOME"));
 	else if (str_cmp(cmd[1], "-") == 0)
 	{
-		value = chdir(getenv("OLDPWD"));
+	value = chdir(getenv("OLDPWD"));
 	}
 	else
-		value = chdir(cmd[1]);
+	value = chdir(cmd[1]);
 
 	if (value == -1)
 	{
-		perror("hsh");
-		return (-1);
+	perror("hsh");
+	return (-1);
 	}
 	else if (value != -1)
 	{
-		getcwd(cwd, sizeof(cwd));
-		setenv("OLDPWD", getenv("PWD"), 1);
-		setenv("PWD", cwd, 1);
+	getcwd(cwd, sizeof(cwd));
+	setenv("OLDPWD", getenv("PWD"), 1);
+	setenv("PWD", cwd, 1);
 	}
 	return (0);
 }
@@ -38,6 +39,7 @@ int change_dir(char **cmd, __attribute__((unused))int st)
  * dis_env - Display enviroment variable
  * @cmd: parsed command
  * @st: status of last command executed
+ *
  * Return: Always 0
  */
 int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int st)
@@ -47,9 +49,9 @@ int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int st)
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		len = str_len(environ[i]);
-		write(1, environ[i], len);
-		write(STDOUT_FILENO, "\n", 1);
+	len = str_len(environ[i]);
+	write(1, environ[i], len);
+	write(STDOUT_FILENO, "\n", 1);
 	}
 	return (0);
 }
@@ -58,6 +60,7 @@ int dis_env(__attribute__((unused)) char **cmd, __attribute__((unused)) int st)
  * echo_bul - execute echo cases
  * @st: statue of last command executed
  * @cmd: parsed command
+ *
  * Return: Always 1 Or execute normal echo
  */
 int echo_bul(char **cmd, int st)
@@ -67,23 +70,23 @@ int echo_bul(char **cmd, int st)
 
 	if (strn_cmp(cmd[1], "$?", 2) == 0)
 	{
-		print_number_int(st);
-		PRINT("\n");
+	print_number_int(st);
+	PRINT("\n");
 	}
 	else if (strn_cmp(cmd[1], "$$", 2) == 0)
 	{
-		print_number(pid);
-		PRINT("\n");
+	print_number(pid);
+	PRINT("\n");
 	}
 	else if (strn_cmp(cmd[1], "$PATH", 5) == 0)
 	{
-		path = get_env("PATH");
-		PRINT(path);
-		PRINT("\n");
-		free(path);
+	path = get_env("PATH");
+	PRINT(path);
+	PRINT("\n");
+	free(path);
 	}
 	else
-		return (print_echo(cmd));
+	return (print_echo(cmd));
 
 	return (1);
 }
@@ -92,6 +95,7 @@ int echo_bul(char **cmd, int st)
  * history_dis - display history of user input on simple_shell
  * @c: parsed command
  * @st: status of last command executed
+ *
  * Return: 0 success or -1 if fail
  */
 int history_dis(__attribute__((unused))char **c, __attribute__((unused))int st)
@@ -106,19 +110,19 @@ int history_dis(__attribute__((unused))char **c, __attribute__((unused))int st)
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 	{
-		return (-1);
+	return (-1);
 	}
 	while ((getline(&line, &len, fp)) != -1)
 	{
-		counter++;
-		a = ito_a(counter);
-		PRINT(a);
-		free(a);
-		PRINT(" ");
-		PRINT(line);
+	counter++;
+	a = ito_a(counter);
+	PRINT(a);
+	free(a);
+	PRINT(" ");
+	PRINT(line);
 	}
 	if (line)
-		free(line);
+	free(line);
 	fclose(fp);
 	return (0);
 }
